908f82ffa08d7fc7ff00d7b08cc5a660601b8800
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-27 22:58:43 +0800

    Delete T3.py

--- T3.py
+++
@@ -1,8 +0,0 @@
-#Drawing and Writing on Images using cv2
-import cv2
-import numpy as np
-img=cv2.imread('green.jpg')
-cv2.line(img,(0,0),(200,300),(255,255,255),10)
-cv2.imshow('Result after drawing a line',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
